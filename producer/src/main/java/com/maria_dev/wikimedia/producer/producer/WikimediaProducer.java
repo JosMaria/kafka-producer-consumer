5ada4cee09a0f1bf1ad1ec94d57d96784983e5a6
JosMaria <josmariaguilar@gmail.com>
2024-08-28 15:57:37 -0400

    consuming stream of wikimedia

---
+++ producer/src/main/java/com/maria_dev/wikimedia/producer/producer/WikimediaProducer.java
@@ -0,0 +1,16 @@
+package com.maria_dev.wikimedia.producer.producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WikimediaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("wikimedia-stream", message);
+    }
+}
