3a6ba1a827c8a0f2893cdd06ef67d19d8372f29f
JosMaria <josmariaguilar@gmail.com>
2024-08-28 23:39:07 -0400

    configure consumer

--- producer/src/main/java/com/maria_dev/wikimedia/producer/stream/WikimediaStreamConsumer.java
+++ producer/src/main/java/com/maria_dev/wikimedia/producer/stream/WikimediaStreamConsumer.java
@@ -22,6 +22,7 @@ public class WikimediaStreamConsumer {
                 .uri("/stream/recentchange")
                 .retrieve()
                 .bodyToFlux(String.class)
-                .subscribe(log::info);
+                //.subscribe(log::info);
+                .subscribe(producer::sendMessage);
     }
 }
